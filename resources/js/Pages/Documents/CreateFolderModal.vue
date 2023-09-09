ed62181867d6e69795bba709df929edc19a5046c
haopengsun <sunhaopeng2016@gmail.com>
2023-09-09 12:30:14 -0400

    Modify create folder modal

--- resources/js/Pages/Documents/CreateFolderModal.vue
+++ resources/js/Pages/Documents/CreateFolderModal.vue
@@ -1,15 +1,24 @@
 <template>
-  <modal :show="modelValue" max-width="sm">
+  <modal :show="modelValue" @show="onShow" max-width="sm">
     <div class="p-6">
         <h2 class="text-lg font-medium text-gray-900">
             Create New Folder
         </h2>
         <div class="mt-6">
             <InputLabel for="folderName" value="Folder Name" class="sr-only"/>
-            <TextInput type="text"/>
+            <TextInput 
+              type="text"
+              ref="folderNameInput"
+              id="folderName"
+              class="mt-1 block w-full"
+              placeholder="Folder Name"
+            />
         </div>
         <div class="mt-6 flex justify-end">
-            <SecondaryButton @click="closeModal">Cancel</SecondaryButton>
+          <SecondaryButton @click="closeModal">Cancel</SecondaryButton>
+          <PrimaryButton class="ml-3" @click="createFolder">
+            Submit
+          </PrimaryButton>
         </div>
     </div>
   </modal>
@@ -20,15 +29,29 @@ import Modal from "@/Components/Modal.vue";
 import InputLabel from "@/Components/InputLabel.vue";
 import TextInput from "@/Components/TextInput.vue";
 import SecondaryButton from "@/Components/SecondaryButton.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import { ref } from 'vue';
 
 // Props & Emit
 const {modelValue} = defineProps({
     modelValue: Boolean
-})
-const emit = defineEmits(['update:modelValue'])
+});
+const emit = defineEmits(['update:modelValue']);
+
+// Refs
+const folderNameInput = ref(null);
 
 // Methods
 function closeModal() {
   emit('update:modelValue', false);
+}
+
+function onShow() {
+  nextTick(() => folderNameInput.value.focus());
+}
+
+function createFolder() {
+  console.log("Create folder");
+  closeModal();
 }
 </script>
